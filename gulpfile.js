be66d62d43bfdb1f7c0a8fa1db5add6759dc24e4
Andrew Myers <maskedcoder@hush.com>
2016-09-27 16:49:33 -0500

    Create task to deal with HTML

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,38 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var frontMatter = require('gulp-front-matter');
+var marked = require('gulp-markdown');
+var layout = require('gulp-layout');
+var pygments = require('pygmentize-bundled');
 
 // Compile CSS
 gulp.task('styles', function () {
   gulp.src('app/styles/main.scss')
     .pipe(sass({
       precision: 10
-	  }))
+    }))
     .pipe(autoprefixer({
       browsers: ['last 2 versions']
     }))
     .pipe(gulp.dest('dist/styles'));
 });
 
-gulp.task('default', ['styles']);
+gulp.task('html', function() {
+  gulp.src('content/**/*.md')
+    .pipe(frontMatter())
+    .pipe(marked({
+      smartypants: true,
+      highlight: function (code, lang, callback) {
+        pygments({ lang: lang, format: 'html' }, code, function (err, result) {
+          callback(err, result.toString());
+        });
+      }
+    }))
+    .pipe(layout(function (file) {
+      return file.frontMatter;
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['styles', 'html']);
